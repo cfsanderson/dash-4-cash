8ebe02926ef7a0717b0d89ce93d4535d3c9960b6
Caleb Frost Sanderson <calebsanderson@gmail.com>
2017-01-26 15:42:54 -0500

    Added some styling and background images to section headers but broke the video somehow...

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,7 @@ class Home extends Component {
           <video autoPlay loop muted poster={posterURL} id='video-bg'>
             <source src={videoURL} type='video/mp4' />
           </video>
+
           <nav className='home-nav'>
             <ul>
               <li>What is D4C?</li>
@@ -21,43 +22,56 @@ class Home extends Component {
               <li>Get Started</li>
             </ul>
           </nav>
+
           <h1>Dash 4 Cash</h1>
           <button>log in with Strava</button>
-          {/* <img src='../images/buttons/btn_strava_connectwith_orange.svg' /> */}
           <div className='explore'>
             <p>Explore</p>
-            <a href='https://simplynoise.com/' className='chevron'>
+            <a href='https://simplynoise.com/' target='_blank' className='chevron'>
               <i className='fa fa-chevron-down faa-float animated' aria-hidden='true' />
             </a>
           </div>
+
         </header>
+
+        <div className='section big whatisD4C'>
+          <h2>What is <br />Dash 4 Cash?</h2>
+        </div>
         <section>
           <div>
-            <img src='../images/Man-Running-Beach1.jpg' />
-          </div>
-          <div>
-            <h2>What is Dash 4 Cash?</h2>
             <p>What is your motivation to get in shape? Health? Beauty? Personal achievement? Those are great reasons but all of them have the same thing in common, YOU!</p>
             <p>What if you could achieve your fitness goals AND help others in need? What if you could also help your friends to meet their goals all while raising funds and awareness for causes that you care about? Now you can.</p>
             <p>Dash for Cash is a social fitness platform that helps people maintain a healthy lifestyle by connecting their fitness goals to a greater purpose. So how does it work?”</p>
           </div>
         </section>
-        <section>
+
+        <div className='section small header1'>
           <h3>Donate</h3>
+        </div>
+        <section>
           <p>Choose a charity that your group would like to support. If you need help, here is a list of some great charities to get you started.”</p>
           <p>At the beginning of each “Dash” you will be asked to make a donation. We make it easy to pay using your debit card or PayPal and we accept donations starting at only $2. Your giving amounts are private but the group’s total donation amount will be made available for all to see.</p>
         </section>
-        <section>
+
+        <div className='section small header2'>
           <h3>Dash</h3>
+        </div>
+        <section>
           <p>Now go for a run! Use your Strava app* to track and log your activity just like normal. When you finish and upload your workout, D4C will sync your Strava data automatically.</p>
           <p>* or whatever app you use to track your workouts as long as it syncs to Strava.</p>
         </section>
-        <section>
+
+        <div className='section small header3'>
           <h3>Track</h3>
+        </div>
+        <section>
           <p>Log into D4C and see personal stats as well as your group’s progress. Dashes can last for a day, a week, or a month and are based on either total distance or total moving time* during that period. You will feel great as you watch your fitness grow along with your dollars. Every run now has a purpose and the potential to change not only your life but also the life of someone in need.</p>
         </section>
-        <section>
+
+        <div className='section small header4'>
           <h3>Ready to dash?</h3>
+        </div>
+        <section>
           <button>log in with Strava</button>
         </section>
         <footer>
