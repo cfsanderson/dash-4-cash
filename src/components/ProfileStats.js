64ad3589b8fbc99511efd74c4e3133ca5a313412
Jason L Perry <jasper@ambethia.com>
2017-02-07 00:41:10 -0500

    Strava Stats

--- src/components/ProfileStats.js
+++ src/components/ProfileStats.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 import strava from '../utils/strava'
+
+const MM = 0.000621371 // Miles per meter
+
+window.moment = moment
+
+// Hard-coded goals for bar graph, adjust as needed
+// TODO: (Base this on something less arbitrary, e.g. personal best)
+const GOALS = {
+  MILES: {
+    WEEK: 10,
+    MONTH: 50
+  },
+  MINUTES: {
+    WEEK: 60,
+    MONTH: 300
+  }
+}
 
 export default class ProfileStats extends Component {
 
   state = {
-    activities: []
+    stats: {
+      miles: {
+        today: 0,
+        thisWeek: 0,
+        lastWeek: 0,
+        thisMonth: 0,
+        lastMonth: 0
+      },
+      minutes: {
+        today: 0,
+        thisWeek: 0,
+        lastWeek: 0,
+        thisMonth: 0,
+        lastMonth: 0
+      }
+    }
   }
 
   componentDidMount () {
-    strava.get('/athlete/activities?per_page=60').then(activities => this.setState({ activities }))
+    const ranges = {
+      today: [moment().subtract(1, 'day'), moment()],
+      thisWeek: [moment().startOf('week'), moment()],
+      lastWeek: [moment().startOf('week').subtract(1, 'week'), moment().startOf('week')],
+      thisMonth: [moment().startOf('month'), moment()],
+      lastMonth: [moment().startOf('month').subtract(1, 'month'), moment().startOf('month')]
+    }
+    strava.get('/athlete/activities?per_page=60').then(activities => {
+      const stats = { minutes: {}, miles: {} }
+      Object.keys(ranges).forEach(range => {
+        const filtered = activities.filter(a => moment(a.start_date).isBetween(...ranges[range]))
+        stats.minutes[range] = filtered.reduce((acc, act) => acc + act.moving_time / 60, 0)
+        stats.miles[range] = (filtered.reduce((acc, act) => acc + act.distance, 0) * MM).toFixed(1)
+      }, {})
+      this.setState({ stats })
+    })
+  }
+
+  // n percent of m (rounded and clamped to 100)
+  per (n, m) {
+    return Math.min(Math.round((n / m) * 100), 100)
   }
 
   render () {
-    // console.log(this.state)
+    const { miles, minutes } = this.state.stats
     return (
       <div className='myStats tables'>
         <h3>My Stats</h3>
@@ -27,7 +80,7 @@ export default class ProfileStats extends Component {
           <tbody className='myStats-section'>
             <tr className='leaderboard-member'>
               <td><h5>Today</h5></td>
-              <td>3 miles</td>
+              <td>{miles.today} miles</td>
             </tr>
           </tbody>
           <tbody className='myStats-section'>
@@ -37,8 +90,8 @@ export default class ProfileStats extends Component {
             <tr>
               <td className='leaderboard-bar' colSpan='3'>
                 <div className='bar'>
-                  <div className='meter' style={{width: '85%'}}>
-                    <p>29.75 miles</p>
+                  <div className='meter' style={{width: `${this.per(miles.thisWeek, GOALS.MILES.WEEK)}%`}}>
+                    <p>{miles.thisWeek} miles</p>
                   </div>
                 </div>
               </td>
@@ -49,8 +102,8 @@ export default class ProfileStats extends Component {
             <tr>
               <td className='leaderboard-bar' colSpan='3'>
                 <div className='bar'>
-                  <div className='meter' style={{width: '60%'}}>
-                    <p>21 miles</p>
+                  <div className='meter' style={{width: `${this.per(miles.lastWeek, GOALS.MILES.WEEK)}%`}}>
+                    <p>{miles.lastWeek} miles</p>
                   </div>
                 </div>
               </td>
@@ -63,8 +116,8 @@ export default class ProfileStats extends Component {
             <tr>
               <td className='leaderboard-bar' colSpan='3'>
                 <div className='bar'>
-                  <div className='meter' style={{width: '85%'}}>
-                    <p>29.75 miles</p>
+                  <div className='meter' style={{width: `${this.per(miles.thisMonth, GOALS.MILES.MONTH)}%`}}>
+                    <p>{miles.thisMonth} miles</p>
                   </div>
                 </div>
               </td>
@@ -75,8 +128,8 @@ export default class ProfileStats extends Component {
             <tr>
               <td className='leaderboard-bar' colSpan='3'>
                 <div className='bar'>
-                  <div className='meter' style={{width: '60%'}}>
-                    <p>21 miles</p>
+                  <div className='meter' style={{width: `${this.per(miles.lastMonth, GOALS.MILES.MONTH)}%`}}>
+                    <p>{miles.lastMonth} miles</p>
                   </div>
                 </div>
               </td>
@@ -94,7 +147,7 @@ export default class ProfileStats extends Component {
           <tbody>
             <tr className='leaderboard-member'>
               <td><h5>Today</h5></td>
-              <td>30 minutes</td>
+              <td>{minutes.today} minutes</td>
             </tr>
           </tbody>
           <tbody>
@@ -104,8 +157,8 @@ export default class ProfileStats extends Component {
             <tr>
               <td className='leaderboard-bar' colSpan='3'>
                 <div className='bar'>
-                  <div className='meter' style={{width: '85%'}}>
-                    <p>200 minutes</p>
+                  <div className='meter' style={{width: `${this.per(minutes.thisWeek, GOALS.MINUTES.WEEK)}%`}}>
+                    <p>{Math.round(minutes.thisWeek)} minutes</p>
                   </div>
                 </div>
               </td>
@@ -116,8 +169,8 @@ export default class ProfileStats extends Component {
             <tr>
               <td className='leaderboard-bar' colSpan='3'>
                 <div className='bar'>
-                  <div className='meter' style={{width: '60%'}}>
-                    <p>324 minutes</p>
+                  <div className='meter' style={{width: `${this.per(minutes.lastWeek, GOALS.MINUTES.WEEK)}%`}}>
+                    <p>{Math.round(minutes.lastWeek)} minutes</p>
                   </div>
                 </div>
               </td>
@@ -130,8 +183,8 @@ export default class ProfileStats extends Component {
             <tr>
               <td className='leaderboard-bar' colSpan='3'>
                 <div className='bar'>
-                  <div className='meter' style={{width: '85%'}}>
-                    <p>1678 minutes</p>
+                  <div className='meter' style={{width: `${this.per(minutes.thisMonth, GOALS.MINUTES.MONTH)}%`}}>
+                    <p>{Math.round(minutes.thisMonth)} minutes</p>
                   </div>
                 </div>
               </td>
@@ -142,8 +195,8 @@ export default class ProfileStats extends Component {
             <tr>
               <td className='leaderboard-bar' colSpan='3'>
                 <div className='bar'>
-                  <div className='meter' style={{width: '60%'}}>
-                    <p>2017 minutes</p>
+                  <div className='meter' style={{width: `${this.per(minutes.lastMonth, GOALS.MINUTES.MONTH)}%`}}>
+                    <p>{Math.round(minutes.lastMonth)} minutes</p>
                   </div>
                 </div>
               </td>
